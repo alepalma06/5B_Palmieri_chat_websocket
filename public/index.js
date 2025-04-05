92da138d1938a5a9366b238d2804757433c3d541
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2025-04-05 19:09:45 +0200

    modifica file  con lista di chi si disconnette e con nome personalizzatoà

--- public/index.js
+++ public/index.js
@@ -1,11 +1,32 @@
 const input = document.getElementById("input");
 const button = document.getElementById("sendButton");
 const chat = document.getElementById("chat");
+const input_nome = document.getElementById("input_nome");
+const button_nome = document.getElementById("sendButton_nome");
+const output = document.getElementById("output");
 
 const template = "<li class=\"list-group-item\">%MESSAGE</li>";
 const messages = [];
-
 const socket = io();
+
+button_nome.onclick = () => {
+    if(input_nome.value==""){
+        console.log("non valido")
+    }
+    else{
+        socket.emit("connessione", input_nome.value)
+        input_nome.classList.add("hidden");
+        input_nome.classList.remove("visible");
+        button_nome.classList.add("hidden");
+        button_nome.classList.remove("visible");
+        input.classList.add("visible");
+        input.classList.remove("hidden");
+        button.classList.add("visible");
+        button.classList.remove("hidden");
+        chat.classList.add("visible");
+        chat.classList.remove("hidden");
+    }
+}
 
 input.onkeydown = (event) => {
   
@@ -26,6 +47,11 @@ socket.on("chat", (message) => {
   render();
 })
 
+socket.on("render_user", (message) => {
+    console.log(message);
+    render_2(message)
+  })
+
 const render = () => {
   let html = "";
   messages.forEach((message) => {
@@ -34,4 +60,14 @@ const render = () => {
   });
   chat.innerHTML = html;
   window.scrollTo(0, document.body.scrollHeight);
+}
+
+
+const render_2 = (message) =>{
+    let html = "User: \n";
+    message.forEach((mess) => {
+        const row = template.replace("%MESSAGE", mess);
+        html+=row;
+      });
+      output.innerHTML = html;
 }
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const http = require("http");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-const { Server } = require('socket.io');
+const { Server } = require('socket.io'); // importazione oggetto Server da socket.io
 const conf = JSON.parse(fs.readFileSync("./conf.json"));
+
+let list = []
 
 app.use(bodyParser.json());
 app.use(
@@ -18,16 +20,27 @@ app.use("/", express.static(path.join(__dirname, "public")));
 const server = http.createServer(app);
 const io = new Server(server);
 io.on('connection', (socket) => {
-   console.log("socket connected: " + socket.id);
-   io.emit("chat", "new client: " + socket.id);
-   socket.on('message', (message) => {
-      const response = socket.id + ': ' + message;
+socket.on('connessione', (message) => {
+    console.log("socket connected: " + socket.id);
+    io.emit("chat", "new client: " + message);
+    socket.nome = message;
+    list.push(socket.nome)
+    console.log(list)
+    io.emit("render_user",list)
+ });
+socket.on('message', (message) => {
+      const response = socket.nome + ': ' + message;
       console.log(response);
       io.emit("chat", response);
    });
+socket.on('disconnect', () => {
+    io.emit("chat", "disconected: " + socket.nome);
+    list = list.filter(element => element !== socket.nome);
+    io.emit("render_user",list)
 });
+});
+
 
 server.listen(conf.port, () => {
   console.log("server running on port: " + conf.port);
-
 });
